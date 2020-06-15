33ad443ddb6f93755223a9652b8e5d8d867dd243
Kenneth Mensah <kennethmensah@Kenneths-MacBook-Pro.local>
2020-06-14 20:56:49 -0700

    add open panel script

---
+++ Assets/Scripts/Panel_open.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_open : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void Open_panel()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+
+            Panel.SetActive(!isActive);
+        }
+    }
+}
